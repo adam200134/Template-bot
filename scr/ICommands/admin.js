27bb96b50218692b9d925e34dea64949f8dbe8c9
adam200134 <adam200134@gmail.com>
2022-05-04 17:22:46 +0800

    OK

--- scr/ICommands/admin.js
+++ scr/ICommands/admin.js
@@ -23,18 +23,12 @@ new ICommandBuilder()
         }
     })
     .setName('create-button').setDescription('發送按鈕訊息').setDefaultPermission(false)
-    .addStringOption(option => option.setName('selbtn').setDescription('選擇按鈕').setRequired(true)
-        .addChoices({ name: "會限申請", value: "ytmember" }, { name: "檢舉", value: "report" }))
     .setexec(async (interaction) => {
         const Permit = interaction.memberPermissions
         const btnid = interaction.options.getString('selbtn');
         const btn = new MessageButton().setCustomId(btnid)
         if (!Permit.has("MANAGE_ROLES") && !Permit.has("ADMINISTRATOR")) return
-        if (btnid == 'ytmember') {
-            btn.setLabel('會限申請').setStyle('PRIMARY')
-        } else if (btnid == 'report') {
-            btn.setLabel('檢舉').setStyle('PRIMARY')
-        }
+        btn.setLabel('檢舉').setStyle('PRIMARY')
         interaction.reply({ content: 'ㄜ 這裡不能打指令ㄌ 按按鈕', components: [new MessageActionRow().addComponents(btn)] })
     })
 
